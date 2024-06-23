7b0b1682ebdadafedf936bcdf29853b1e7c1a401
jsurajba <jsurajba@stevens.edu>
2024-06-22 23:09:09 -0400

    Update Get Patent IDs - 2024.R
    Fixed since get_bulk_patent_data overwrites rather than appends to .csv file

--- Get Patent IDs - 2024.R
+++ Get Patent IDs - 2024.R
@@ -1,3 +1,8 @@
 library(patentr)
 
-get_bulk_patent_data(year = rep(2022, 52), week = 1:52, output_file = "2024-data.csv")
+weeks <- 1:52
+
+for(week_num in weeks){
+  file <- paste0("path/2024-", week_num, "-data.csv")
+  try(get_bulk_patent_data(year = 2024, week = week_num, output_file = file), silent = FALSE)
+}
